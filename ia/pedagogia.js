8d0eacfc6d00fc57588166de39b5e12852f1c735
tayrot <tayrot@gmail.com>
2026-01-30 17:04:31 -0300

    Add files via upload

---
+++ ia/pedagogia.js
@@ -0,0 +1,240 @@
+// pedagogia.js
+// Mapa pedagógico – 1º ano – Português (Brasil)
+
+export const pedagogia = {
+
+  /* =========================
+     VOGAIS E BASE
+  ========================= */
+
+  "vogais": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "reconhecimento-letras",
+    dataset: {
+      letras: ["A", "E", "I", "O", "U"]
+    }
+  },
+
+  "consoantes": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "reconhecimento-letras",
+    dataset: {
+      letras: ["B","C","D","F","G","L","M","N","P","R","S","T","V"]
+    }
+  },
+
+  /* =========================
+     FAMÍLIAS SILÁBICAS
+  ========================= */
+
+  "silabas-com-b": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "completar-palavras",
+    dataset: {
+      nivel1: [
+        "bola","bala","bolo","bico","bota",
+        "bebe","banana","bala","boca","bala"
+      ],
+      nivel2: [
+        "barco","boneca","bicicleta","balde","batata",
+        "borboleta","barriga","bebida","balão","berço"
+      ]
+    }
+  },
+
+  "silabas-com-c": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "completar-palavras",
+    dataset: {
+      nivel1: [
+        "casa","copo","cola","cubo","cama",
+        "cano","capa","caco","cera","cinto"
+      ],
+      nivel2: [
+        "cavalo","cadeira","camisa","cachorro","coelho",
+        "caderno","cidade","cenoura","cinema","coruja"
+      ]
+    }
+  },
+
+  "silabas-com-d": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "completar-palavras",
+    dataset: {
+      nivel1: [
+        "dado","dedo","doce","dente","dia",
+        "dona","dino","dama","duna","dica"
+      ],
+      nivel2: [
+        "dinheiro","dentista","doceria","domingo","doente",
+        "desenho","dinossauro","doceira","dançar","doçura"
+      ]
+    }
+  },
+
+  "silabas-com-f": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "completar-palavras",
+    dataset: {
+      nivel1: [
+        "faca","fogo","fila","fita","foca",
+        "fada","foto","feno","fala","fino"
+      ],
+      nivel2: [
+        "familia","fazenda","formiga","foguete","farmacia",
+        "figura","floresta","fechadura","fantasia","festival"
+      ]
+    }
+  },
+
+  "silabas-com-g": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "completar-palavras",
+    dataset: {
+      nivel1: [
+        "gato","galo","gota","goma","gelo",
+        "gude","gari","goma","gibi","gula"
+      ],
+      nivel2: [
+        "girafa","galinha","geladeira","goiaba","garrafa",
+        "gorila","goleiro","gravata","guitarra","gordura"
+      ]
+    }
+  },
+
+  "silabas-com-l": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "completar-palavras",
+    dataset: {
+      nivel1: [
+        "lata","lua","lobo","lama","lupa",
+        "lado","lima","lago","luva","lixo"
+      ],
+      nivel2: [
+        "ladeira","lagarto","limonada","livro","leitura",
+        "lampada","lanchonete","lousa","liquidificador","lavadeira"
+      ]
+    }
+  },
+
+  "silabas-com-m": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "completar-palavras",
+    dataset: {
+      nivel1: [
+        "mala","mesa","mola","muro","moto",
+        "maca","mato","meia","mina","mimo"
+      ],
+      nivel2: [
+        "macaco","menina","montanha","marmita","melancia",
+        "morango","mercado","medalha","moradia","motorista"
+      ]
+    }
+  },
+
+  "silabas-com-n": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "completar-palavras",
+    dataset: {
+      nivel1: [
+        "nave","ninho","nome","nuvem","nariz",
+        "nota","nada","neta","noite","nuca"
+      ],
+      nivel2: [
+        "navio","natureza","natal","nene","neve",
+        "novelo","noticia","nascimento","namorado","numeral"
+      ]
+    }
+  },
+
+  "silabas-com-p": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "completar-palavras",
+    dataset: {
+      nivel1: [
+        "pato","pipa","pote","pato","pena",
+        "pano","pulo","peixe","pata","pico"
+      ],
+      nivel2: [
+        "panela","pipoca","professor","parquinho","palhaço",
+        "pescador","pintura","passarinho","planeta","pedreiro"
+      ]
+    }
+  },
+
+  "silabas-com-r": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "completar-palavras",
+    dataset: {
+      nivel1: [
+        "rato","rosa","rede","rua","roda",
+        "rico","riso","rama","rola","rima"
+      ],
+      nivel2: [
+        "raposa","robotica","relampago","recreio","roupa",
+        "rodovia","refeicao","relatorio","restaurante","resgate"
+      ]
+    }
+  },
+
+  "silabas-com-s": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "completar-palavras",
+    dataset: {
+      nivel1: [
+        "sapo","sala","sol","suco","sino",
+        "saco","seda","sapo","selo","soma"
+      ],
+      nivel2: [
+        "sapato","sorvete","sabado","segredo","saudade",
+        "sementinha","seringa","sacola","sabonete","surpresa"
+      ]
+    }
+  },
+
+  "silabas-com-t": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "completar-palavras",
+    dataset: {
+      nivel1: [
+        "tatu","taco","teto","tubo","tina",
+        "tela","toma","taco","teto","toalha"
+      ],
+      nivel2: [
+        "tomate","telefone","tesoura","tartaruga","torneira",
+        "tabuleiro","travesseiro","temperatura","teclado","televisao"
+      ]
+    }
+  },
+
+  "silabas-com-v": {
+    serie: "1-ano",
+    materia: "portugues",
+    tipo: "completar-palavras",
+    dataset: {
+      nivel1: [
+        "vaca","vela","vida","vivo","vaso",
+        "voo","vala","vila","vela","voto"
+      ],
+      nivel2: [
+        "violao","ventilador","viagem","vizinho","vitamina",
+        "vassoura","verdura","vestido","veterinario","video"
+      ]
+    }
+  }
+
+};
